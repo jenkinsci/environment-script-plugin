09bfbfe74f768ad2f79b68582b860696dd38e42c
BobDu <i@bobdu.cc>
2024-02-24 16:25:57 +0800

    Use jelly tag select replace html select
    Signed-off-by: BobDu <i@bobdu.cc>

--- src/main/java/com/lookout/jenkins/commands/Commands.java
+++ src/main/java/com/lookout/jenkins/commands/Commands.java
@@ -2,8 +2,14 @@ package com.lookout.jenkins.commands;
 
 public class Commands {
 
+    public final static String UNIX_SCRIPT = "unixScript";
+    public final static String UNIX_SCRIPT_DISPLAY_NAME = "Unix script";
+
     public final static String POWER_SHELL = "powerShell";
+    public final static String PROWER_SHELL_DISPLAY_NAME = "Powershell script";
+
     public final static String BATCH_SCRIPT = "batchScript";
+    public final static String BATCH_SCRIPT_DISPLAY_NAME = "Batch script";
 
     public static boolean isShebangs(String script) {
         if (script.startsWith("#!")) {
